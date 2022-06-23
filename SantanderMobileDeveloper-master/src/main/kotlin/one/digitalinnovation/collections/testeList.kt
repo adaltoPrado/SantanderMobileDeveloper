9d7d19b688e9ea1e56b57ae04e6892a8e2d47d4f
Adalto Prado <adaltoprado@proton.me>
2022-06-23 11:45:27 -0300

    Created map test class

---
+++ SantanderMobileDeveloper-master/src/main/kotlin/one/digitalinnovation/collections/testeList.kt
@@ -0,0 +1,32 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0,  "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+    println("--------------------------")
+
+    println(funcionarios.find { it.nome == "Maria" })
+    println("--------------------------")
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
+    println("--------------------------")
+
+    funcionarios.groupBy { it.tipoDeContratacao }.forEach { println(it) }
+
+
+}
+data class Funcionario(
+    val nome:String,
+    val salario:Double,
+    val tipoDeContratacao:String
+){
+    override fun toString(): String = """
+        Nome: $nome
+        Salário: $salario
+    """.trimIndent()
+}
